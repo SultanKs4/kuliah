bfee47490d7f86dd1a362615f9fd1ac10d4aade9
SultanKs4 <sultangendut@gmail.com>
2019-03-20 08:18:53 +0700

    add for UI

--- Java/NetBeans Project/AlgoritmaStruktuData/src/JS5Tugas/SeqSTotal.java
+++ Java/NetBeans Project/AlgoritmaStruktuData/src/JS5Tugas/SeqSTotal.java
@@ -17,6 +17,15 @@ public class SeqSTotal {
     }
 
     public static void input() {
+        System.out.println("Menghitung jumlah suatu bilangan dalam array");
+        System.out.print("Data : { ");
+        for (int i = 0; i < data.length; i++) {
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(data[i]);
+        }
+        System.out.println(" }");
         System.out.print("Masukkan nilai yang dicari : ");
         cari = scanInt.nextInt();
     }
