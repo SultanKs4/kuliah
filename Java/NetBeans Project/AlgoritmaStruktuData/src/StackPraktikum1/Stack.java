1eab46812e9721e9dcf8a099ebd5eada08f1ab05
SultanKs4 <sultangendut@gmail.com>
2019-03-21 20:37:57 +0700

    Stack Praktikum 1

---
+++ Java/NetBeans Project/AlgoritmaStruktuData/src/StackPraktikum1/Stack.java
@@ -0,0 +1,75 @@
+package StackPraktikum1;
+
+/**
+ * @author Sultan
+ * @category Stack
+ */
+public class Stack {
+    int size, top = -1;
+    int[] data;
+
+    public Stack(int size) {
+        this.size = size;
+        data = new int[size];
+        top = -1;
+    }
+
+    public boolean IsEmpty() {
+        if (top == -1)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean IsFull() {
+        if (top == size - 1)
+            return true;
+        else
+            return false;
+    }
+
+    public void push(int dt) {
+        if (!IsFull()) {
+            top++;
+            data[top] = dt;
+        } else {
+            System.out.println("===================================");
+            System.out.println("Isi Stack Penuh");
+        }
+    }
+
+    public void pop() {
+        if (!IsEmpty()) {
+            int a = data[top];
+            top--;
+            System.out.println("===================================");
+            System.out.println("Data yang di Pop : " + a);
+        } else {
+            System.out.println("===================================");
+            System.out.println("Tidak dapat Pop, Stack masih kosong");
+        }
+    }
+
+    public void tampilkan() {
+        System.out.println("===================================");
+        System.out.println("Isi Stack : ");
+        for (int i = top; i >= 0; i--) {
+            System.out.println("[" + data[i] + "] ");
+        }
+        System.out.println("===================================");
+    }
+
+    public void clearStack() {
+        if (!IsEmpty()) {
+            for (int i = top; i >= 0; i--) {
+                top--;
+            }
+
+            System.out.println("===================================");
+            System.out.println("Stack Berhasil dikosongkan");
+        } else {
+            System.out.println("===================================");
+            System.out.println("Tidak dapat Hapus, Stack Masih Kosong");
+        }
+    }
+}
---
+++ Java/NetBeans Project/AlgoritmaStruktuData/src/StackPraktikum1/TestStack.java
@@ -0,0 +1,21 @@
+package StackPraktikum1;
+
+/**
+ * TestStack
+ */
+public class TestStack {
+    public static void main(String[] args) {
+        Stack tumpuk = new Stack(5);
+
+        tumpuk.push(20);
+        tumpuk.push(21);
+        tumpuk.push(12);
+        tumpuk.push(19);
+        tumpuk.push(11);
+
+        tumpuk.tampilkan();
+
+        tumpuk.pop();
+        tumpuk.tampilkan();
+    }
+}
