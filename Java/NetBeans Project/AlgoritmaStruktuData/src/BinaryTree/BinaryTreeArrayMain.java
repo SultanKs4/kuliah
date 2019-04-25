ddd54e8d05a71881061c18ae3083da89a58c0068
SultanKs4 <sultangendut@gmail.com>
2019-04-26 00:57:35 +0700

    JS 12 Binary Tree Praktikum

---
+++ Java/NetBeans Project/AlgoritmaStruktuData/src/BinaryTree/BinaryTreeArrayMain.java
@@ -0,0 +1,16 @@
+package BinaryTree;
+
+/**
+ * @author Sultan
+ * @see BinaryTreeArrayMain
+ */
+public class BinaryTreeArrayMain {
+    public static void main(String[] args) {
+        BinaryTreeArray bta = new BinaryTreeArray();
+        int[] data = { 6, 4, 8, 3, 5, 7, 9, 0, 0, 0 };
+        int idxLast = 6;
+        bta.populateData(data, idxLast);
+        bta.traverseInOrder(0);
+        System.out.println("");
+    }
+}
---
+++ Java/NetBeans Project/AlgoritmaStruktuData/src/BinaryTree/Node.java
@@ -0,0 +1,20 @@
+package BinaryTree;
+
+/**
+ * @author Sultan
+ * @see Node
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node() {
+    }
+
+    public Node(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
