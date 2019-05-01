acc8788769d281f751358ddd6aa05b9a1073ee76
SultanKs4 <sultangendut@gmail.com>
2019-05-01 11:25:19 +0700

    add menu Tugas 5

--- Java/NetBeans Project/AlgoritmaStruktuData/src/BinaryTree/BinaryTreeMain.java
+++ Java/NetBeans Project/AlgoritmaStruktuData/src/BinaryTree/BinaryTreeMain.java
@@ -1,33 +1,69 @@
 package BinaryTree;
 
+import java.util.Scanner;
+
 /**
  * @author Sultan
  * @see BinaryTreeMain
  */
 public class BinaryTreeMain {
+    static Scanner scan = new Scanner(System.in);
+    static Scanner scan1 = new Scanner(System.in);
+    static BinaryTree bt = new BinaryTree();
+    static int data;
 
     public static void main(String[] args) {
-        BinaryTree bt = new BinaryTree();
+        try {
+            while (true)
+                menu();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-        bt.addRecursive(6);
-        bt.addRecursive(4);
-        bt.addRecursive(8);
-        bt.addRecursive(3);
-        bt.addRecursive(5);
-        bt.addRecursive(7);
-        bt.addRecursive(9);
-        bt.addRecursive(10);
-        bt.addRecursive(15);
+    public static void menu() throws Exception {
+        System.out.println("BINARY TREE MENU");
+        System.out.println("1. Add\n2. Delete\n3. Find\n4. Traverse inOrder");
+        System.out.println("5. Traverse preOrder\n6. Traverse postOrder\n7. Keluar");
+        System.out.println("================================");
+        System.out.print(">> ");
+        int pilihan = scan.nextInt();
+        System.out.println("================================");
 
-        bt.traversePreOrder(bt.root);
-        System.out.println("");
-        bt.traverseInOrder(bt.root);
-        System.out.println("");
-        bt.traversePostOrder(bt.root);
-        System.out.println("");
-        System.out.println("Min : " + bt.min());
-        System.out.println("Max : " + bt.max());
-        bt.leaf();
-        System.out.println("Jumlah Leaf : " + bt.countLeaf());
+        switch (pilihan) {
+        case 1:
+            System.out.print("Masukkan data : ");
+            data = scan.nextInt();
+            bt.addRecursive(data);
+            break;
+        case 2:
+            System.out.print("Masukkan data : ");
+            data = scan.nextInt();
+            bt.delete(data);
+            break;
+        case 3:
+            System.out.print("Masukkan data : ");
+            data = scan.nextInt();
+            System.out.println("Find : " + bt.find(data));
+            break;
+        case 4:
+            bt.traverseInOrder(bt.root);
+            System.out.println();
+            break;
+        case 5:
+            bt.traversePreOrder(bt.root);
+            System.out.println();
+            break;
+        case 6:
+            bt.traversePostOrder(bt.root);
+            System.out.println();
+            break;
+        case 7:
+            throw new Exception("Keluar");
+        default:
+            System.out.println("Input Invalid");
+            break;
+        }
+        System.out.println();
     }
 }
